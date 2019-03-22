b5c1db285a83796847f2035ee502a67ca6f8b632
Valmirthecoder <valmir.haxholli@student.cacttus.education>
2019-03-22 17:09:12 +0100

    Added Minor changes, changed pic when we roll dice

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores,roundScore,activePlayer,dice;
+var scores,roundScore,activePlayer;
 
 scores = [0,0];
 roundScore = 0;
@@ -17,3 +17,16 @@ activePlayer = 1;
 dice = Math.floor(Math.random() * 6) + 1;
 
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent='0';
+document.getElementById('current-0').textContent='0';
+document.getElementById('score-1').textContent='0';
+document.getElementById('current-1').textContent='0';
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    var diceDOM=document.querySelector('.dice');
+    diceDOM.style.display='block';
+    diceDOM.src = 'dice-'+dice+'.png';
+
+});
