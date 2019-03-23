3f8a03aeb41e52f68d7b767e17ec7faed0ff361f
Valmirthecoder <valmir.haxholli@student.cacttus.education>
2019-03-23 21:46:09 +0100

    Changing scores and toggle active player

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var scores,roundScore,activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 dice = Math.floor(Math.random() * 6) + 1;
 
@@ -28,5 +28,22 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
     var diceDOM=document.querySelector('.dice');
     diceDOM.style.display='block';
     diceDOM.src = 'dice-'+dice+'.png';
+
+    if(dice!==1){
+      roundScore+=dice;
+      document.querySelector('#current-'+activePlayer).textContent=roundScore;
+    }
+    else{
+      activePlayer===0?activePlayer=1:activePlayer=0;
+      roundScore=0;
+      document.getElementById('current-0').textContent='0';
+      document.getElementById('current-1').textContent='0';
+
+      document.querySelector('.player-0-panel').classList.toggle('active');  //NESE OSHT AKTIVE HEKE DHE ANASJELLTAS
+      document.querySelector('.player-1-panel').classList.toggle('active');
+
+      document.querySelector('.dice').style.display = 'none';
+
+    }
 
 });
